e8613b57fc02554abe27bfc618b156e9e303bd46
lzugaz <93231358+lzugaz@users.noreply.github.com>
2024-11-01 13:55:44 -0400

    Update main.py

--- backend/main/main.py
+++ backend/main/main.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../../frontend')
 
-@app.route('/')
+def init_db():
+    with sqlite3.connect('data.db') as conn:  
+        cursor = conn.cursor() #Creating table this is only done the first time the data base is made
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS my_table (
+                customer_account INTEGER PRIMARY KEY,
+                SAP_S8 INTEGER NOT NULL,
+                HOTEL_CHAIN TEXT NOT NULL
+            )
+        ''')
+        conn.commit()
+
+def insert_data(name, value):
+    with sqlite3.connect('data.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO my_table (name, value) VALUES (?, ?)', (name, value))#adds the rows of the file to the db
+        conn.commit()
+
+@app.route('/') # Displaying HTML CODE
 def index():
-    return render_template('../../frontend/index.html')
+    return render_template('index.html')
+
+@app.route('/insert', methods=['POST'])#post is giving data to someother place, so here is 
+def insert():
+    name = request.form.get('name')
+    value = request.form.get('value')
+    if name and value:
+        insert_data(name, value)
+        return f"Inserted {name} with value {value} into the database!"
+    return "Failed to insert data."
 
 if __name__ == '__main__':
+    init_db()  
     app.run(debug=True)
